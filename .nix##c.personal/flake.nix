38da42396cfd3a3ad2617745643f5e39002df5ec
unlsycn <unlsycn@unlsycn.com>
2024-08-25 18:15:31 +0800

    feat: add nix configs
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

---
+++ .nix##c.personal/flake.nix
@@ -0,0 +1,25 @@
+# .nix/flake.nix
+{
+  description = "Nix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      myuser = "unlsycn";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations = {
+        "${myuser}" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+        };
+      };
+    };
+}
