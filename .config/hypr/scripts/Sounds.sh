ef220dc05b6b13047678d98996470aca234dad0a
unlsycn <unlsycn@unlsycn.com>
2024-09-14 10:45:00 +0800

    update hypr configs

--- .config/hypr/scripts/Sounds.sh
+++ .config/hypr/scripts/Sounds.sh
@@ -59,9 +59,7 @@ iDIR="$sDIR/../$iTheme"
 
 # Find the sound file and play it.
 sound_file=$(find $sDIR/stereo -name "$soundoption" -print -quit)
-if ! test -f "$sound_file"; then
-    sound_file=$(find $iDIR/stereo -name "$soundoption" -print -quit)
-    if ! test -f "$sound_file"; then
+if ! test -f "$sound_file"; then sound_file=$(find $iDIR/stereo -name "$soundoption" -print -quit) if ! test -f "$sound_file"; then
         sound_file=$(find $userDIR/$defaultTheme/stereo -name "$soundoption" -print -quit)
         if ! test -f "$sound_file"; then
             sound_file=$(find $systemDIR/$defaultTheme/stereo -name "$soundoption" -print -quit)
